1fc3732d2d0a5cb68fc8b4c06968094090ed89e1
Mohammad Fakrul Alam <fakrul@alam.rocks>
2020-01-25 07:35:46 +1100

    change outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "public_ip" {
     value       = aws_instance.bullseye.public_ip
-    description = "The public IP of the web server"
+    description = "The public IP of the web server is: "
 }
