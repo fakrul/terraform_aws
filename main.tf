22a3fe8f0161260f7f8b680dcd0ad8a0f5d0ee53
Mohammad Fakrul Alam <fakrul@alam.rocks>
2020-01-25 07:22:38 +1100

    chagne variables.tf.sample

--- main.tf
+++ main.tf
@@ -6,26 +6,32 @@ resource "aws_security_group" "bullseye" {
     from_port = 80
     to_port = 80
     protocol = "tcp"
+    ipv6_cidr_blocks = ["::/0"]
+  }
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
   }   
   ingress {
-    from_port = 53
-    to_port = 53
+    from_port = 22
+    to_port = 22
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }   
-  ingress {
-    from_port = 53
-    to_port = 53
-    protocol = "udp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }    
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    ipv6_cidr_blocks = ["::/0"]
+  }
   egress {
     from_port = 0
     to_port = 0
@@ -35,12 +41,14 @@ resource "aws_security_group" "bullseye" {
 }
 
 resource "aws_instance" "bullseye" {
-  ami = "ami-0328aad0f6218c429"
+  ami = "ami-05850fe0bf797791c"
   instance_type = "t2.micro"
   user_data = "${file("initial.conf")}"
   security_groups = ["${aws_security_group.bullseye.id}"]
   subnet_id = "subnet-4d89ce28"
+  ipv6_address_count = 1
   tags = {
     Name = "bullseye"
+    "DHCP option" = "alam.rocks"
   }
 }
